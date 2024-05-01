2add54b26f4daa3cde03874af73b55ce621674ba
G VIKRAM <97166893+Vk4011@users.noreply.github.com>
2024-05-01 09:16:46 +0530

    Add files via upload

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../components/Navbar"
+import Search from './Search'
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <Search/>
+    </>
+  )
+}
+
+export default Home
